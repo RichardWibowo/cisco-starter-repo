84b37b7656f27673f5adb07e64c9f13ae56aa09d
richardmwibowo <63839269+RichardWibowo@users.noreply.github.com>
2023-01-15 16:59:19 +0700

    update

--- cisco-web/src/pages/Hero.jsx
+++ cisco-web/src/pages/Hero.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import {useState,useEffect} from 'react'
+import axios from 'axios'
 
 const Hero = () => {
+    //creating IP state
+  const [ip, setIP] = useState('');
+
+  //creating function to load ip address from the API
+  const getData = async () => {
+    const res = await axios.get('https://geolocation-db.com/json/')
+    console.log(res.data);
+    setIP(res.data.IPv4)
+  }
+  
+  useEffect( () => {
+    //passing getData method to the lifecycle method
+    getData()
+
+  }, [])
     return (
+        
         <div className='bg-gradient-to-r from-[#99ffce] to-white-500 '>
         <div className='text-black'>
             <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
@@ -9,7 +27,7 @@ const Hero = () => {
                     Welcome back 
                 </p>  
                 <h1 className= "md:text-7xl sm:text-6xl text-3xl font-bold m:py-6">
-                    #IP address goes here 
+                    {ip}
                 </h1>
                 <div className= 'flex justify-center items-center'>
                     <p className='md:text-1xl sm:text-1xl text-1l font-bold py-4'>
